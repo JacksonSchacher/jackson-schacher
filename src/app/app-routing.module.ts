fd1fa972b2f6553c6dfda83b12820ebb8d3c49b1
JacksonSchacher <jschacher8@gmail.com>
2021-10-28 11:09:06 -0600

    portfolilo setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppDetailsComponent } from './app-details/app-details.component';
+import { ApplicationsComponent } from './applications/applications.component';
+import { ContactComponent } from './contact/contact.component';
+import { DesignComponent } from './design/design.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'applications/details', component: AppDetailsComponent},
+  {path: 'applications', component: ApplicationsComponent},
+  {path: 'designs', component: DesignComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplicationsComponent } from './applications/applications.component';
+import { DesignComponent } from './design/design.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AppDetailsComponent } from './app-details/app-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationsComponent,
+    DesignComponent,
+    ContactComponent,
+    HomeComponent,
+    AppDetailsComponent
   ],
   imports: [
     BrowserModule,
