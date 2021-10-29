e92869f3db0d0238ab370d42107efc054d195176
JacksonSchacher <jschacher8@gmail.com>
2021-10-29 10:45:18 -0600

    need to fix relative routing in app comp

--- src/app/applications/applications.component.ts
+++ src/app/applications/applications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-applications',
@@ -18,8 +19,14 @@ export class ApplicationsComponent implements OnInit {
       heroImg: 'https://www.lambdatest.com/blog/wp-content/uploads/2018/08/534-x-300-20-1.jpg'
     }
   ]
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
+  // goToAppDetails(appTitle: string) {
+  //   this.router.navigate(['application/details/' + appTitle], {relativeTo: this.route});
+  // }
   ngOnInit(): void {
   }
 
